91e1e7ccb4e5cec23143e6cf3f9a8d2586095679
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-06-14 09:49:41 +0200

    feat: sequelize

---
+++ src/sequelize/index.ts
@@ -0,0 +1,33 @@
+import Sequelize from "sequelize";
+
+export const sequelize = new Sequelize.Sequelize({
+    dialect: "sqlite",
+    storage: "../../database.sqlite"
+});
+
+export class Message extends Sequelize.Model {
+    text!: string;
+    author!: string;
+    modifiedAt!: Date;
+    chatId!: string;
+}
+Message.init({
+    text: {
+        type: Sequelize.DataTypes.STRING,
+    },
+    author: {
+        type: Sequelize.DataTypes.STRING,
+    },
+    modifiedAt: {
+        type: Sequelize.DataTypes.DATE
+    },
+    chatId: {
+        type: Sequelize.DataTypes.STRING
+    }
+}, {
+    sequelize,
+    modelName: "Message"
+});
+export async function init(): Promise<void> {
+    await sequelize.sync();
+}
