f8aeb0c1543c15f5f9b607499843a2266b1c32ba
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-06-14 11:10:14 +0200

    feat: simple message

--- src/socket/socketHandler.ts
+++ src/socket/socketHandler.ts
@@ -1,6 +1,17 @@
 import io from 'socket.io';
+import * as database from '../sequelize';
+
 
 
 export default async function handleSocket(socket: io.Socket): Promise<void> {
-    
+    socket.on("message", (text: string) => {
+        const message = {
+            author: socket.data.id,
+            chatId: socket.data.room,
+            text,
+            modifiedAt: new Date()
+        };
+        database.Message.create(message);
+        socket.to(socket.data.room).emit("new_message", message);
+    });
 }
