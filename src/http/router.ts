9daac7765948aba287ad7ebb41571bd483a8a200
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2021-07-03 15:42:46 +0200

    feat: message request

--- src/http/router.ts
+++ src/http/router.ts
@@ -1,7 +1,64 @@
 import http from 'http';
+import * as messageManager from '../messageManager';
+
 
 
 export default async function router(req: http.IncomingMessage, body: string): Promise<Response> {
+    if (!req.url?.startsWith("/api/")) {
+        return {
+            status: 404,
+            response: JSON.stringify({
+                msg: "Resource not found.",
+                possibleFix: "Maybe forgot the /api/url prefix for all api requests."
+            })
+        }
+    }
+    if (req.method != "GET") return {
+        status: 405,
+        response: JSON.stringify({
+            msg: "Method not supported."
+        })
+    }
+    const url = req.url.replace("/api", "");
+    if (url.startsWith("/channel")) {
+        const newUrl = url.replace("/channel", "");
+        const segments = newUrl.split("/").filter(v => v.length > 0);
+        if (segments.length == 0) {
+            return {
+                status: 404,
+                response: JSON.stringify({
+                    msg: "Resource not found.",
+                    possibleFix: "Maybe provide a channel id."
+                })
+            };
+        }
+        if (segments.length == 1) {
+
+        }
+        if (segments.length == 2) {
+            try {
+                const m = await messageManager.getMessage(segments[0], segments[1]);
+                return {
+                    status: 200,
+                    response: JSON.stringify({
+                        message: m
+                    })
+                };
+            } catch (e) {
+                if (!(e instanceof Error)) return {
+                    status: 500,
+                    response: e
+                } 
+                return {
+                    status: 404,
+                    response: JSON.stringify({
+                        msg: e.message,
+                    })
+                }
+            }
+        }
+
+    }
     return {
         status: 404,
         response: JSON.stringify({
--- src/messageManager.ts
+++ src/messageManager.ts
@@ -1,5 +1,8 @@
-export function getMessages(channelId: string): Array<Message> {
+export async function getMessages(channelId: string): Promise<Array<Message>> {
     return [];
+}
+export async function getMessage(channelId: string, messageId: string): Promise<Message> {
+    return new Message();
 }
 export class Message {
 
